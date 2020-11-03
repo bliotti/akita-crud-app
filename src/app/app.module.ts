43314db93e991eb4e0ec6a4acbf2a70d750d805c
Brian Liotti <bliotti@protonmail.com>
2020-11-03 00:11:49 -0500

    akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CreateCourseComponent } from "./course/components/create-course/create-course.component";
+import { CoursesListComponent } from "./course/components/courses-list/courses-list.component";
+import { CourseModule } from "./course/course.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+const routes = [
+  {
+    path: "courses",
+    component: CoursesListComponent,
+  },
+  { path: "create-course", component: CreateCourseComponent },
+  { path: "**", redirectTo: "courses" },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CourseModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/course/store/course.query.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { QueryEntity } from "@datorama/akita";
+import { CourseState, CourseStore } from "./course.store";
+
+@Injectable({ providedIn: "root" })
+export class CourseQuery extends QueryEntity<CourseState> {
+  selectAreCourseLoaded$ = this.select((s) => {
+    return s.areCoursesLoaded;
+  });
+
+  constructor(protected store: CourseStore) {
+    super(store);
+  }
+}
